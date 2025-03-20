25abb2a894d3bb65f2c9f88a325048039c4389dd
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2025-03-20 15:10:38 +0200

    Created test user model

---
+++ src/users/models.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(10))
+    fullname: Mapped[Optional[str]]
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
