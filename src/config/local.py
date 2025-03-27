4d75cedc0c19fff0290e6d327db2da589833ea9f
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2025-03-27 13:42:04 +0200

    Added prefix for local .env params

--- src/config/local.py
+++ src/config/local.py
@@ -4,5 +4,4 @@ from .base import Settings
 
 
 class LocalSettings(Settings):
-    model_config = SettingsConfigDict(env_file=".env.local", env_file_encoding="utf-8", case_sensitive=True)
-    ENVIRONMENT: str = "local"
+    model_config = SettingsConfigDict(env_prefix="LOCAL_")
