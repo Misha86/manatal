d52668acabc87c9919295090693f59f2d6238473
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2025-03-27 16:38:55 +0200

    Updated Configs

--- src/config/base.py
+++ src/config/base.py
@@ -1,10 +1,19 @@
 import secrets
 from pathlib import Path
-from typing import List, Optional, Union
+from typing import List, Union
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from pydantic import AnyHttpUrl, HttpUrl, PostgresDsn, ValidationInfo, field_validator
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import (
+    AnyHttpUrl,
+    PostgresDsn,
+    RedisDsn,
+    ValidationInfo,
+    field_validator,
+)
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict,
+)
 
 from src.core.cache.backends.redis import RedisBytecodeCache
 
@@ -20,12 +29,10 @@ class Settings(BaseSettings):
 
     TEMPLATE_DIRS: list[Path] = [BASE_DIR / "templates"]
 
-    # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
-    # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
-    # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
     @field_validator("BACKEND_CORS_ORIGINS")
+    @classmethod
     def assemble_cors_origins(cls, value: Union[str, List[str]]) -> Union[List[str], str]:
         if isinstance(value, str) and not value.startswith("["):
             return [i.strip() for i in value.split(",")]
@@ -33,50 +40,47 @@ class Settings(BaseSettings):
             return value
         raise ValueError(value)
 
-    SENTRY_DSN: HttpUrl | None = None
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB: str = "manatallocal"
 
-    @field_validator("SENTRY_DSN")
-    def sentry_dsn_can_be_blank(cls, url: str) -> Optional[str]:  # noqa
-        return None if url is None or not url else url
-
-    SQLALCHEMY_DATABASE_URI: str | None = None
+    SQLALCHEMY_DATABASE_URI: PostgresDsn | None = None
 
     @field_validator("SQLALCHEMY_DATABASE_URI")
-    def assemble_db_connection(cls, value: Optional[str], info: ValidationInfo) -> str:
-        if isinstance(value, str):
+    @classmethod
+    def assemble_db_connection(cls, value: str | None, info: ValidationInfo) -> str:
+        if isinstance(value, str) and str(PostgresDsn(value)):
             return value
 
         env_data = info.data
+
         postgres_dsn = PostgresDsn.build(
             scheme="postgresql+asyncpg",
-            username=env_data.get("POSTGRES_USER", "postgres"),
-            password=env_data.get("POSTGRES_PASSWORD", "postgres"),
-            host=env_data.get("POSTGRES_HOST", "localhost"),
-            port=env_data.get("POSTGRES_PORT", 5432),
-            path=env_data.get("POSTGRES_DB", "manatallocal"),
+            username=env_data["POSTGRES_USER"],
+            password=env_data["POSTGRES_PASSWORD"],
+            host=env_data["POSTGRES_HOST"],
+            port=env_data["POSTGRES_PORT"],
+            path=env_data["POSTGRES_DB"],
         )
         return str(postgres_dsn)
-    
-    JINJA2_ENV: Environment | None = None
-    
-    @field_validator("JINJA2_ENV")
-    def jinja2_environment(cls, value: Optional[str], info: ValidationInfo) -> Environment:
-        if isinstance(value, Environment):
-            return value
 
-        env_data = info.data
-        
-        bytecode_cache = RedisBytecodeCache(
-            location=env_data.get("JINJA2_REDIS_HOST", "redis://localhost:6379/0"),
-            key_prefix=env_data.get("JINJA2_CACHE_PREFIX", "jinja2:"),
-            timeout=env_data.get("JINJA2_CACHE_TIMEOUT", 3600),
-        )
-        return Environment(
-            loader=FileSystemLoader(env_data.get("TEMPLATE_DIRS", [])),
-            autoescape=select_autoescape(enabled_extensions=("html",)),
-            cache_size=env_data.get("JINJA2_CACHE_SIZE", 400),
-            bytecode_cache=bytecode_cache,
-            trim_blocks=True,
-            lstrip_blocks=True,
-            enable_async=True,
-        )
+    JINJA2_REDIS_DSN: RedisDsn = "redis://localhost:6379/0"
+    JINJA2_CACHE_SIZE: int = 400
+    JINJA2_CACHE_PREFIX: str = "jinja2:"
+    JINJA2_CACHE_TIMEOUT: int = 3600
+
+    bytecode_cache: RedisBytecodeCache = RedisBytecodeCache(
+        location=JINJA2_REDIS_DSN, key_prefix=JINJA2_CACHE_PREFIX, timeout=JINJA2_CACHE_TIMEOUT
+    )
+
+    JINJA2_ENV: Environment = Environment(
+        loader=FileSystemLoader(TEMPLATE_DIRS),
+        autoescape=select_autoescape(enabled_extensions=("html",)),
+        cache_size=JINJA2_CACHE_SIZE,
+        bytecode_cache=bytecode_cache,
+        trim_blocks=True,
+        lstrip_blocks=True,
+        enable_async=True,
+    )
