3a2b0c4886d173e42c90e8919edf9aec8bf826af
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2025-03-28 18:06:12 +0200

    Added cache to the get_user_html route

--- src/dependencies.py
+++ src/dependencies.py
@@ -6,18 +6,19 @@ from src.config import settings
 from src.core.database import SessionLocal
 
 if TYPE_CHECKING:
-    from typing import AsyncGenerator, AsyncIterator
+    from typing import AsyncGenerator
 
     from redis.asyncio import Redis
     from sqlalchemy.ext.asyncio import AsyncSession
+
 
 async def get_async_session() -> "AsyncGenerator[AsyncSession, None]":
     async with SessionLocal() as session:
         yield session
 
 
-async def get_redis() -> "AsyncIterator[Redis]":
-    session = from_url(url=settings.REDIS_CACHE_URL, encoding="utf-8", decode_responses=True)
+async def get_redis() -> "AsyncGenerator[Redis, None]":
+    session = from_url(url=settings.REDIS_CACHE_URL, encoding="utf-8", decode_responses=False, protocol=3)
     try:
         yield session
     finally:
--- src/users/router.py
+++ src/users/router.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends, Form, Request, UploadFile
 from fastapi.responses import HTMLResponse
 from pydantic import UUID4
+from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.utils import render_to_string
-from src.dependencies import get_async_session
+from src.dependencies import get_async_session, get_redis
 from src.users import schemas, service
 
 router = APIRouter(
@@ -30,7 +31,15 @@ async def create_upload_files(logo: UploadFile, name: str = Form(), session: Asy
 
 
 @router.get("/{user_id}/html", response_class=HTMLResponse)
-async def get_user_html(request: Request, user_id: UUID4, session: AsyncSession = Depends(get_async_session)):
+async def get_user_html(
+    request: Request, user_id: UUID4, session: AsyncSession = Depends(get_async_session), redis: Redis = Depends(get_redis)
+):
     user = await service.get_user(session, user_id) or {}
 
-    return await render_to_string("users/user_info.html", {"user": user}, request)
+    html_name = "users/user_info.html"
+
+    if not (html_page := await redis.get(html_name)):
+        html_page = await render_to_string(html_name, {"user": user}, request)
+        await redis.set(html_name, html_page, ex=10)
+
+    return html_page
