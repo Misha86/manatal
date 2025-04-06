4dceb72c146696e2730e9aadaf3cf64c2bd215ec
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2025-04-06 14:56:51 +0300

    Added validate_jwt_private_key to the LocalSettings

--- src/config/local.py
+++ src/config/local.py
@@ -1,3 +1,6 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from pydantic import field_validator
 from pydantic_settings import SettingsConfigDict
 
 from .base import Settings
@@ -5,3 +8,22 @@ from .base import Settings
 
 class LocalSettings(Settings):
     model_config = SettingsConfigDict(env_prefix="LOCAL_")
+
+    JWT_PRIVATE_KEY: str
+
+    @field_validator("JWT_PRIVATE_KEY")
+    @classmethod
+    def validate_jwt_private_key(cls, value: str) -> str:
+        private_key = value.replace("\\n", "\n")
+
+        if not (
+            private_key.startswith("-----BEGIN PRIVATE KEY-----") or private_key.startswith("-----BEGIN RSA PRIVATE KEY-----")
+        ) or not private_key.strip().endswith("-----END PRIVATE KEY-----"):
+            raise ValueError("Invalid RS256 private key format.")
+
+        try:
+            serialization.load_pem_private_key(private_key.encode(), password=None, backend=default_backend())
+        except Exception as err:
+            raise ValueError(f"Invalid private key: {err}")
+
+        return private_key
