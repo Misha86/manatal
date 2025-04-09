f9d1358824b095c6803f0e7b5e258e824cd2cc16
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2025-04-09 20:48:08 +0300

    Reload status for RequestValidationError

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from fastapi_pagination import add_pagination
 
 from src import career_pages, users
@@ -7,6 +10,12 @@ from src.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME, version="1.0.0")
 add_pagination(app)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError) -> JSONResponse:
+    return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"detail": jsonable_encoder(exc.errors())})
+
 
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
